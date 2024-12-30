93a0b04c4c67aceb4bda28b743bef10b9b668064
galih <samudragalih94@gmail.com>
2024-12-30 10:56:14 +0700

    feat(project_1): load asset on view controller

--- project_1/Project1/Project1/ViewController.swift
+++ project_1/Project1/Project1/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadImages()
+        print("pictures: ", pictures)
     }
 
+    private func loadImages() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
 
 }
 
