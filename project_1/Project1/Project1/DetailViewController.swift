e85feb9b207bcc0c08af7a4cf23545693133a2ef
galih <samudragalih94@gmail.com>
2024-12-31 10:13:43 +0700

    feat(project_1): create detail vc to contain image

---
+++ project_1/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Galih Samudra on 31/12/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
