4e15cf5e09b023552257f856bafc57825b6cf4f9
galih <samudragalih94@gmail.com>
2025-01-02 10:33:17 +0700

    feat(project_2): show flags inside button

--- project_2/Project2/Project2/ViewController.swift
+++ project_2/Project2/Project2/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadCountries()
+        askQuestions()
+        setButtons()
     }
-
-
+    
+    private func setButtons() {
+        let buttons = [button1, button2, button3]
+        buttons.forEach { button in
+            button?.layer.borderWidth = 1
+            button?.layer.borderColor = UIColor.lightGray.cgColor
+            button?.imageView?.contentMode = .scaleToFill
+            button?.clipsToBounds = true
+        }
+    }
+    
+    private func loadCountries() {
+        countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    private func askQuestions() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 }
 
