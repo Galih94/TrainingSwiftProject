c7911a261e290f628d29e2698cdf0f7037f7df8c
galih <samudragalih94@gmail.com>
2025-01-06 11:24:34 +0700

    feat(project_3): copy project 1 into project_3

---
+++ project_3/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Galih Samudra on 31/12/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    public var selectedImage: String?
+    public var selectedTitle: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadImage()
+        setTitle(selectedTitle)
+    }
+    
+    private func setTitle(_ text: String?) {
+        title = text
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        shouldHideBarOnTap(true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        shouldHideBarOnTap(false)
+    }
+
+    private func loadImage() {
+        guard let imageName = selectedImage else { return }
+        imageView.image = UIImage(named: imageName)
+    }
+    
+    private func shouldHideBarOnTap(_ isHide: Bool) {
+        navigationController?.hidesBarsOnTap = isHide
+    }
+}
